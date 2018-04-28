48704a4a4bbc9745dc78479ed345496dfcc762d5
Tagumi <cmcjnz@gmail.com>
2018-04-28 21:32:22 +1200

    Added JavaDocs for all classes

--- src/kalah/Store.java
+++ src/kalah/Store.java
@@ -2,6 +2,10 @@ package kalah;
 
 public class Store extends Pit {
 
+    /***
+     * Constructor for Store subclass
+     * @param owner the owner of this Store
+     */
     public Store(PlayerID owner) {
         super(owner);
         this.beanCount = 0;
